2eed29ba0104e0f1a41919b18a9a8f02f1bafaf7
CLICC User <smwlks@hotmail.com>
2015-10-22 14:41:39 -0700

    for k-means homework

--- euc.py
+++ euc.py
@@ -4,11 +4,20 @@ points = [(2, 10), (2, 5), (8, 4), (5, 8), (7, 5), (6, 4), (1, 2), (4, 9)]
 
 coord = [];
 
-for i in range (0, 7):
+for i in range (0, 8):
     coord.append(numpy.array(points[i]))
-    
-for j in range (0, 7):
-    for k in range (j, 7):
-        print (str(numpy.linalg.norm(coord[j]-coord[k])) + " ")
+
+#for j in range (0, 8):
+ #   for k in range (j, 8):
+  #      print (str(numpy.linalg.norm(coord[j]-coord[k])) + " ")
+   # print ("\n")
+
+k1 = numpy.array((11/3, 9)) #(3,9.5) (2,10)
+k2 = numpy.array((7,13/3))  # (6.5,5.25) (6,6)
+k3 = numpy.array((1.5,3)) # (1.5,3) (1.5,3)
+
+for i in range (0, 8):
+    print (numpy.linalg.norm(k1-coord[i]))
+    print (numpy.linalg.norm(k2-coord[i]))
+    print (numpy.linalg.norm(k3-coord[i]))
     print ("\n")
-    
